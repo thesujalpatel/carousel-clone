91094544a15530860638bfa739c8f5eee0d2f152
thesujalpatel <patelsujal2412004@gmail.com>
2025-09-15 18:05:57 +0530

    Added rest of the components

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Hello Wolrd");
+  const button = document.querySelector("#menu-btn");
+  const collapsibleNav = document.querySelector(".nav-collapsible");
+
+  let menuOpen = false;
+
+  button.addEventListener("click", function () {
+    if (menuOpen) {
+      collapsibleNav.style.height = collapsibleNav.scrollHeight + "px";
+      setTimeout(function () {
+        collapsibleNav.style.height = "0px";
+      }, 1);
+    } else {
+      collapsibleNav.style.height = collapsibleNav.scrollHeight + "px";
+      setTimeout(function () {
+        if (menuOpen) {
+          collapsibleNav.style.height = "auto";
+        }
+      }, 300);
+    }
+    menuOpen = !menuOpen;
+  });
 });
